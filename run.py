69c002ecb0b70c914aff6d9baa124b709827eedd
Franul <f.sapikowski2@gmail.com>
2020-08-12 23:24:41 +0200

    Added run file

---
+++ run.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+from sys import argv, stderr
+
+if __name__ == '__main__':
+    bans_path = Path(argv[1])
+    champion_path = Path(argv[2])
+    output_path = Path(argv[3])
+    team_to_recommend = int(Path(argv[4]))
+
+
+    if not bans_path.exists():
+        print(f'{bans_path} does not exist!', file=stderr)
+        exit(-1)
+    if not champion_path.exists():
+        print(f'{champion_path} does not exist!', file=stderr)
+        exit(-1)
+
+    if not output_path.exists():
+        output_path.mkdir(parents=True, exist_ok=True)
+
+    rec_dict, pos_picks = create_recommendations(bans_path, champion_path,
+                                                 output_path, team_to_recommend)
+    recom_path = output_path / 'recommendations.txt'
+    with open(recom_path, 'w') as f:
+        for key, value in pos_picks.items():
+            f.write('%s: %f\n' % key, value)
