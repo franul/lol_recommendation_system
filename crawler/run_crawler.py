f527464265ed2b46e896bd3f9e90db5e4973233f
Franul <f.sapikowski2@gmail.com>
2020-08-20 00:57:37 +0200

    Removeing some typos

--- crawler/run_crawler.py
+++ crawler/run_crawler.py
@@ -13,8 +13,8 @@ if __name__ == '__main__':
     begin_time = int((datetime.today() - timedelta(6)).timestamp()) * 1000
     end_time = int(datetime.today().timestamp()) * 1000
     for region in regions:
-        folder_path = os.path.join(os.getcwd(), folder_name)
+        folder_path = os.path.join(os.getcwd(), region)
         league_dict = riot_crawler.fetch_league(region)
-        summoner_ids, accounts, account_ids = riot_crawler.fetch_acc_ids(region, league_dict, save=False, destination_path=folder_path)
-        match_ids, account_ids_done = riot_crawler.fetch_match_ids(region, account_ids, begin_time, end_time, save=False, destination_path=folder_path)
-        match_list, match_id_dones, account_ids_unseen = riot_crawler.fetch_match_info(region, match_ids, save=False, destination_path=folder_path)
+        summoner_ids, accounts, account_ids = riot_crawler.fetch_acc_ids(region, league_dict, save=True, destination_path=folder_path)
+        match_ids, account_ids_done = riot_crawler.fetch_match_ids(region, account_ids, begin_time, end_time, save=True, destination_path=folder_path)
+        match_list, match_id_dones, account_ids_unseen = riot_crawler.fetch_match_info(region, match_ids, save=True, destination_path=folder_path)
