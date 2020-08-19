0b2f013c03b661d9ab2988d978dd6f4733decbf5
Franul <f.sapikowski2@gmail.com>
2020-08-20 00:33:36 +0200

    Added regions and saving

--- crawler/run_crawler.py
+++ crawler/run_crawler.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python3
 from datetime import date, timedelta, datetime
 from crawler import Riot_Crawler
+import os
 
 if __name__ == '__main__':
     load = True
     api_key = ''
     queue = 'RANKED_SOLO_5x5'
     queue_id = 420
-    region = 'NA1'
+    regions = ['EUN1', 'EUW1', 'NA1', 'KR', 'BR1']
     riot_crawler = Riot_Crawler(api_key, queue, queue_id)
     begin_time = int((datetime.today() - timedelta(6)).timestamp()) * 1000
     end_time = int(datetime.today().timestamp()) * 1000
-    league_dict = riot_crawler.fetch_league(region)
-    summoner_ids, accounts, account_ids = riot_crawler.fetch_acc_ids(region, league_dict)
-    match_ids, account_ids_done = riot_crawler.fetch_match_ids(region, account_ids, begin_time, end_time)
-    match_list, match_id_dones, account_ids_unseen = riot_crawler.fetch_match_info(region, match_ids)
+    for region in regions:
+        folder_path = os.path.join(os.getcwd(), folder_name)
+        league_dict = riot_crawler.fetch_league(region)
+        summoner_ids, accounts, account_ids = riot_crawler.fetch_acc_ids(region, league_dict, save=False, destination_path=folder_path)
+        match_ids, account_ids_done = riot_crawler.fetch_match_ids(region, account_ids, begin_time, end_time, save=False, destination_path=folder_path)
+        match_list, match_id_dones, account_ids_unseen = riot_crawler.fetch_match_info(region, match_ids, save=False, destination_path=folder_path)
