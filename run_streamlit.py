2ea045fe238ff3c71176199067e8a940f350029f
Franul <f.sapikowski2@gmail.com>
2020-08-23 16:03:20 +0200

    Added multiselection lists

--- run_streamlit.py
+++ run_streamlit.py
@@ -8,3 +8,18 @@ path = os.path.join(data_path, 'champion.json')
 with open(path, 'r') as f:
     champions = json.load(f)
 poss_champ_list = [item for item in champions['data'].keys()]
+
+bans = st.multiselect("Choose bans (10 max):", poss_champ_list)
+if len(bans) > 10:
+    st.error("There are more than 10 bans.")
+team100 = st.multiselect("Choose champions for team blue:", [item for item in poss_champ_list if item not in bans])
+team200 = st.multiselect("Choose champions for team purple:", [item for item in poss_champ_list if item not in bans])
+
+if not team100 and not team100:
+    st.error("Please select at least one champion.")
+
+if len(team100) > 5:
+    st.error("Team blue has more than 5 champions.")
+
+if len(team200) > 5:
+    st.error("Team purple has more than 5 champions.")
