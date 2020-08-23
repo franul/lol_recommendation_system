9715d9913894f96cc59d8a1b817354b5db38f43f
Franul <f.sapikowski2@gmail.com>
2020-08-23 16:34:25 +0200

    Added model recommendation

--- run_streamlit.py
+++ run_streamlit.py
@@ -1,21 +1,24 @@
 import streamlit as st
 import os
 import json
+from create_recommendations import create_recommendations
 
-
-data_path = os.path.join(os.path.abspath(os.getcwd()), 'data')
-path = os.path.join(data_path, 'champion.json')
-with open(path, 'r') as f:
-    champions = json.load(f)
-poss_champ_list = [item for item in champions['data'].keys()]
-
+@st.cache
+def load_champions():
+    data_path = os.path.join(os.path.abspath(os.getcwd()), 'data')
+    path = os.path.join(data_path, 'champion.json')
+    with open(path, 'r') as f:
+        champions = json.load(f)
+    poss_champ_list = [item for item in champions['data'].keys()]
+    return poss_champ_list
+poss_champ_list = load_champions()
 bans = st.multiselect("Choose bans (10 max):", poss_champ_list)
 if len(bans) > 10:
     st.error("There are more than 10 bans.")
 team100 = st.multiselect("Choose champions for team blue:", [item for item in poss_champ_list if item not in bans])
 team200 = st.multiselect("Choose champions for team purple:", [item for item in poss_champ_list if item not in bans])
 
-if not team100 and not team100:
+if not team100 and not team200:
     st.error("Please select at least one champion.")
 
 if len(team100) > 5:
@@ -23,3 +26,14 @@ if len(team100) > 5:
 
 if len(team200) > 5:
     st.error("Team purple has more than 5 champions.")
+champion_list = dict(zip(team100 + team200, [100 for _ in team100] + [200 for _ in team200]))
+rec_dict, pos_picks = create_recommendations(bans, champion_list)
+
+if pos_picks[100]:
+    st.write('### Recommendations for team blue:')
+    for item in pos_picks[100]:
+        st.write('{}: {}'.format(item[0], round(item[1], 3)))
+if pos_picks[200]:
+    st.write('### Recommendations for team purple:')
+    for item in pos_picks[100]:
+        st.write('{}: {}'.format(item[0], round(item[1], 3)))
