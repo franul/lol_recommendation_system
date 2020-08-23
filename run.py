ce0b36d2304f00c49a4cdb24c84ed2215336f2e3
Franul <f.sapikowski2@gmail.com>
2020-08-23 16:35:07 +0200

    Changed arguments of function

--- run.py
+++ run.py
@@ -21,8 +21,41 @@ if __name__ == '__main__':
     if not output_path.exists():
         output_path.mkdir(parents=True, exist_ok=True)
 
-    rec_dict, pos_picks = create_recommendations(bans_path, champion_path,
-                                                 output_path)
+    #loading champion bans and names
+    bans = []
+    with open(bans_path) as f:
+        lines = f.readlines()
+        for line in lines:
+            ban = line.strip()
+            try:
+                ban = int(ban)
+            except:
+                pass
+            bans.append(ban)
+
+    #loading champion bans and names
+    champions = []
+    teams = []
+    with open(champion_path) as f:
+        lines = f.readlines()
+        for line in lines:
+            line = line.strip()
+            words = line.split()
+            champion = words[0]
+            try:
+                champion = int(champion)
+            except:
+                pass
+            team = words[1]
+            try:
+                team = int(team)
+            except:
+                pass
+            champions.append(champion)
+            teams.append(team)
+    champion_dict = dict(zip(champions, teams))
+
+    rec_dict, pos_picks = create_recommendations(bans, champion_dict)
     recom_path = output_path / 'recommendations.txt'
     with open(recom_path, 'w') as f:
         if pos_picks[100]:
